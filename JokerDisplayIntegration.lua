0649f58c0e52ca1a92fe333e48bb776f4e2a787e
OppositeWolf770 <shschool42@gmail.com>
2024-07-05 15:50:47 -0500

    (Ice Blow) Modify description

--- JokerDisplayIntegration.lua
+++ JokerDisplayIntegration.lua
@@ -5,22 +5,24 @@ jd_def = JokerDisplay.Definitions
 
 -- Ice Blow
 jd_def["j_twewy_iceBlow"] = {
-    line_2 = {
+    line_1 = {
         {
-            text = "(",
-            colour = G.C.UI.TEXT_INACTIVE,
-            scale = 0.3
+            text = "+",
+            colour = lighten(G.C.RED, 0.1)
         },
         {
             ref_table = "card.joker_display_values",
-            ref_value = "active_text",
-            scale = 0.3
+            ref_value = "aDiscards",
+            colour = lighten(G.C.RED, 0.1)
         },
+    },
+    line_2 = {
+
         {
-            text = ")",
+            text = "(Discards)",
             colour = G.C.UI.TEXT_INACTIVE,
             scale = 0.3
-        },
+        }
     },
 
     calc_function = function(card)
@@ -41,14 +43,8 @@ jd_def["j_twewy_iceBlow"] = {
 
             card.joker_display_values.active = (faces >= 3) and true or false
         end
-
-        card.joker_display_values.active_text = card.joker_display_values.active and "Active!" or "Inactive"
-    end,
-
-    style_function = function(card, line_1, line_2)
-        if line_2 then
-            line_2.children[2].config.colour = card.joker_display_values.active and G.C.GREEN or G.C.UI.TEXT_INACTIVE
-        end
+        
+        card.joker_display_values.aDiscards = card.joker_display_values.active and 3 or 0
     end
 }
 
