f204cac2a4fdfcc03a71450b2a35a8695d5dc5f3
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-07-04 23:36:38 -0500

    (Ice Risers) Add JokerDisplay integration

--- JokerDisplayIntegration.lua
+++ JokerDisplayIntegration.lua
@@ -52,4 +52,47 @@ jd_def["j_twewy_iceBlow"] = {
     end
 }
 
+jd_def["j_twewy_iceRisers"] = {
+    line_2 = {
+        {
+            text = "(",
+            colour = G.C.UI.TEXT_INACTIVE,
+            scale = 0.3
+        },
+        {
+            ref_table = "card.joker_display_values",
+            ref_value = "active_text",
+            scale = 0.3
+        },
+        {
+            text = ")",
+            colour = G.C.UI.TEXT_INACTIVE,
+            scale = 0.3
+        },
+    },
+
+    calc_function = function(card)
+        local hand = next(G.play.cards) and G.play.cards or G.hand.highlighted
+        local _, _, scoring_hand = JokerDisplay.evaluate_hand(hand)
+        local faces = false
+
+        -- Checks the scoring_hand for a face card
+        for k, v in pairs(scoring_hand) do
+            if v:is_face() and not v.debuff then
+                faces = true
+                break
+            end
+        end
+
+        card.joker_display_values.active = (faces and G.GAME.current_round.discards_left > 0) and true or false
+        card.joker_display_values.active_text = card.joker_display_values.active and "Active!" or "Inactive"
+    end,
+
+    style_function = function(card, line_1, line_2)
+        if line_2 then
+            line_2.children[2].config.colour = card.joker_display_values.active and G.C.GREEN or G.C.UI.TEXT_INACTIVE
+        end
+    end
+}
+
 ------------------------------------------------------------------------
