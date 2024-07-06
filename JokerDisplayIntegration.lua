a579d03ea3fb0e1fdc8f58d098d7f4caa21fd56f
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-07-05 21:01:27 -0500

    (Ice Risers) Modify description

--- JokerDisplayIntegration.lua
+++ JokerDisplayIntegration.lua
@@ -50,31 +50,31 @@ jd_def["j_twewy_iceBlow"] = {
 
 -- Ice Risers
 jd_def["j_twewy_iceRisers"] = {
-    line_2 = {
+    line_1 = {
         {
-            text = "(",
-            colour = G.C.UI.TEXT_INACTIVE,
-            scale = 0.3
+            text = "+",
+            colour = lighten(G.C.CHIPS, 0.1)
         },
         {
             ref_table = "card.joker_display_values",
-            ref_value = "active_text",
-            scale = 0.3
-        },
+            ref_value = "aHands",
+            colour = lighten(G.C.CHIPS, 0.1)
+        }
+    },
+    line_2 = {
         {
-            text = ")",
+            text = "(Hands)",
             colour = G.C.UI.TEXT_INACTIVE,
             scale = 0.3
-        },
+        }
     },
 
     calc_function = function(card)
         local hand = next(G.play.cards) and G.play.cards or G.hand.highlighted
-        local _, _, scoring_hand = JokerDisplay.evaluate_hand(hand)
         local faces = false
 
         -- Checks the scoring_hand for a face card
-        for k, v in pairs(scoring_hand) do
+        for k, v in pairs(hand) do
             if v:is_face() and not v.debuff then
                 faces = true
                 break
@@ -82,13 +82,7 @@ jd_def["j_twewy_iceRisers"] = {
         end
 
         card.joker_display_values.active = (faces and G.GAME.current_round.discards_left > 0) and true or false
-        card.joker_display_values.active_text = card.joker_display_values.active and "Active!" or "Inactive"
-    end,
-
-    style_function = function(card, line_1, line_2)
-        if line_2 then
-            line_2.children[2].config.colour = card.joker_display_values.active and G.C.GREEN or G.C.UI.TEXT_INACTIVE
-        end
+        card.joker_display_values.aHands = card.joker_display_values.active and 1 or 0
     end
 }
 
